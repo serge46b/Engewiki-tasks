811471de419275e970ff99db20c44e773b55ff22
сергей <esmery@ngs.ru>
2020-11-02 03:28:22 +0700

    pre-game operations completed

--- ship battle.py
+++ ship battle.py
@@ -25,7 +25,7 @@ y_size = playGround_size_x // field_size_y
 
 bricks = []
 
-ship_dict = {"2": [0, 0, 0, 0, 2], "3": [0, 0, 0, 0, 2], "4": [0, 0, 0, 0, 1]}
+ship_dict = {"2": [0, 0, 0, 0, 2, 0], "3": [0, 0, 0, 0, 2, 0], "4": [0, 0, 0, 0, 1, 0]}
 selected_ship = 0
 ship_placed = 0
 ship_rotation = 0
@@ -35,6 +35,7 @@ y_ship_prev = 0
 x_ship_size_prev = 0
 y_ship_size_prev = 0
 step_prev = 1
+event_prev = None
 
 
 def place_ship(x, y, x_ship_size, y_ship_size):
@@ -50,14 +51,14 @@ def place_ship(x, y, x_ship_size, y_ship_size):
         step = 1
         if x_ship_size < 0 or y_ship_size < 0:
             step = -1
-        for i in range(x, x+x_ship_size, step):
-            for g in range(y, y+y_ship_size, step):
+        for i in range(x, x + x_ship_size, step):
+            for g in range(y, y + y_ship_size, step):
                 brick = bricks[g][i]
                 if brick.b_type == "ship":
                     deny = 1
         if deny == 0:
-            for i in range(x, x+x_ship_size, step):
-                for g in range(y, y+y_ship_size, step):
+            for i in range(x, x + x_ship_size, step):
+                for g in range(y, y + y_ship_size, step):
                     brick = bricks[g][i]
                     brick.b_type = "ship"
                     brick.draw(canvas)
@@ -66,7 +67,13 @@ def place_ship(x, y, x_ship_size, y_ship_size):
             x_ship_size_prev = x_ship_size
             y_ship_size_prev = y_ship_size
             step_prev = step
-            ship_placed = True
+            ship_placed = 1
+        else:
+            for i in range(x_ship_prev, x_ship_prev + x_ship_size_prev, step_prev):
+                for g in range(y_ship_prev, y_ship_prev + y_ship_size_prev, step_prev):
+                    brick = bricks[g][i]
+                    brick.b_type = "ship"
+                    brick.draw(canvas)
 
 
 def interface_clicked(event):
@@ -74,9 +81,12 @@ def interface_clicked(event):
     ship_rotation += 90
     if ship_rotation > 270:
         ship_rotation = 0
+    clicked(event_prev)
 
 
 def clicked(event):
+    global event_prev
+    event_prev = event
     if selected_ship != 0:
         x_brick = event.x // x_size - field_centering_x
         y_brick = event.y // y_size - field_centering_y
@@ -90,16 +100,16 @@ def clicked(event):
             denied_fields_y = selected_ship - 1
         elif ship_rotation == 270:
             denied_fields_x = selected_ship - 1
-        #print(denied_fields_x, denied_fields_y)
+        # print(denied_fields_x, denied_fields_y)
         deny = 1
         if 1 <= x_brick <= field_size_x and 1 <= y_brick <= field_size_y:
             if denied_fields_x >= 0 and denied_fields_y >= 0:
                 if denied_fields_x < x_brick <= field_size_x and denied_fields_y < y_brick <= field_size_y:
-                    #print("ok", x_brick, y_brick)
+                    # print("ok", x_brick, y_brick)
                     deny = 0
             else:
                 if 1 <= x_brick <= field_size_x + denied_fields_x and 1 <= y_brick <= field_size_y + denied_fields_y:
-                    #print("ok", x_brick, y_brick)
+                    # print("ok", x_brick, y_brick)
                     deny = 0
             if deny == 0:
                 if ship_rotation >= 180:
@@ -109,50 +119,70 @@ def clicked(event):
                     denied_fields_x -= 1
                     denied_fields_y -= 1
                 place_ship(x_brick, y_brick, -1 * denied_fields_x, -1 * denied_fields_y)
+                draw_interface_ship(ship_dict["4"][0], ship_dict["4"][1], ship_dict["4"][2], ship_dict["4"][3], False,
+                                    canvas)
 
 
 def ship_selected(event):
     global ship_dict
     global selected_ship
-    global ship_placed
+    global x_ship_prev, y_ship_prev, x_ship_size_prev, y_ship_size_prev, step_prev, ship_placed, event_prev
     if ship_dict["4"][0] <= event.x <= ship_dict["4"][2] and ship_dict["4"][1] <= event.y <= ship_dict["4"][3]:
-        if selected_ship != 4 and selected_ship != 0 and ship_placed != 0:
+        if selected_ship != 0 and ship_placed == 1:
             ship_dict[str(selected_ship)][4] -= 1
             ship_placed = 0
-        selected_ship = 4
+            x_ship_prev, y_ship_prev, x_ship_size_prev, y_ship_size_prev, event_prev = 0, 0, 0, 0, 0
+            step_prev = 1
         if ship_dict["4"][4] > 0:
+            selected_ship = 4
             draw_interface_ship(ship_dict["4"][0], ship_dict["4"][1], ship_dict["4"][2], ship_dict["4"][3], True,
                                 canvas)
             draw_interface_ship(ship_dict["3"][0], ship_dict["3"][1], ship_dict["3"][2], ship_dict["3"][3], False,
                                 canvas)
             draw_interface_ship(ship_dict["2"][0], ship_dict["2"][1], ship_dict["2"][2], ship_dict["2"][3], False,
                                 canvas)
+        else:
+            draw_interface_ship(ship_dict["4"][0], ship_dict["4"][1], ship_dict["4"][2], ship_dict["4"][3], False,
+                                canvas)
+            selected_ship = 0
 
     elif ship_dict["3"][0] <= event.x <= ship_dict["3"][2] and ship_dict["3"][1] <= event.y <= ship_dict["3"][3]:
-        if selected_ship != 3 and selected_ship != 0 and ship_placed != 0:
+        if selected_ship != 0 and ship_placed == 1:
             ship_dict[str(selected_ship)][4] -= 1
             ship_placed = 0
-        selected_ship = 3
+            x_ship_prev, y_ship_prev, x_ship_size_prev, y_ship_size_prev, event_prev = 0, 0, 0, 0, 0
+            step_prev = 1
         if ship_dict["3"][4] > 0:
+            selected_ship = 3
             draw_interface_ship(ship_dict["4"][0], ship_dict["4"][1], ship_dict["4"][2], ship_dict["4"][3], False,
                                 canvas)
             draw_interface_ship(ship_dict["3"][0], ship_dict["3"][1], ship_dict["3"][2], ship_dict["3"][3], True,
                                 canvas)
             draw_interface_ship(ship_dict["2"][0], ship_dict["2"][1], ship_dict["2"][2], ship_dict["2"][3], False,
                                 canvas)
+        else:
+            draw_interface_ship(ship_dict["3"][0], ship_dict["3"][1], ship_dict["3"][2], ship_dict["3"][3], False,
+                                canvas)
+            selected_ship = 0
 
     elif ship_dict["2"][0] <= event.x <= ship_dict["2"][2] and ship_dict["2"][1] <= event.y <= ship_dict["2"][3]:
-        if selected_ship != 2 and selected_ship != 0 and ship_placed != 0:
+        if selected_ship != 0 and ship_placed == 1:
             ship_dict[str(selected_ship)][4] -= 1
             ship_placed = 0
-        selected_ship = 2
+            x_ship_prev, y_ship_prev, x_ship_size_prev, y_ship_size_prev, event_prev = 0, 0, 0, 0, 0
+            step_prev = 1
         if ship_dict["2"][4] > 0:
+            selected_ship = 2
             draw_interface_ship(ship_dict["4"][0], ship_dict["4"][1], ship_dict["4"][2], ship_dict["4"][3], False,
                                 canvas)
             draw_interface_ship(ship_dict["3"][0], ship_dict["3"][1], ship_dict["3"][2], ship_dict["3"][3], False,
                                 canvas)
             draw_interface_ship(ship_dict["2"][0], ship_dict["2"][1], ship_dict["2"][2], ship_dict["2"][3], True,
                                 canvas)
+        else:
+            draw_interface_ship(ship_dict["2"][0], ship_dict["2"][1], ship_dict["2"][2], ship_dict["2"][3], False,
+                                canvas)
+            selected_ship = 0
 
 
 class Brick:
@@ -184,6 +214,7 @@ class Brick:
             idx = cnvs.index(txt, tk.END)
             cnvs.insert(txt, idx, text)
         else:
+            border_color = ""
             if self.b_type == "field":
                 fill_color = "cyan"
                 border_color = "white"
@@ -223,13 +254,15 @@ def draw_interface_ship(x, y, x2, y2, is_selected, cnvs):
         border_color = "green"
     else:
         border_color = "white"
-    cnvs.create_rectangle(x, y, x2, y2, fill="red",
-                          outline=border_color, tags="click" + str(x))
-    txt = cnvs.create_text((x + x2) // 2, (y + y2) // 2,
-                           font="Arial 12",
-                           fill="white", tags="click" + str(x))
+    cnvs.create_rectangle(x, y, x2, y2, fill="red", outline=border_color, tags="click" + str(x))
+    txt = cnvs.create_text((x + x2) // 2, (y + y2) // 2, font="Arial 12", fill="white", tags="click" + str(x))
     idx = cnvs.index(txt, tk.END)
     cnvs.insert(txt, idx, (y2 - y) // 20)
+    lbl = ship_dict[str((y2 - y) // 20)][5]
+    color = "black"
+    if ship_dict[str((y2 - y) // 20)][4] == 0:
+        color = "gray"
+    cnvs.itemconfig(lbl, fill=color, text=str(ship_dict[str((y2 - y) // 20)][4]) + "x")
     cnvs.tag_bind("click" + str(x), "<Button-1>", ship_selected)
 
 
@@ -241,10 +274,15 @@ def draw_interface(x_cnvs_size, y_cnvs_size, cnvs):
     x = x_cnvs_size - 10
     y = y_cnvs_size - 80
 
+    lbl = cnvs.create_text((x + x_cnvs_size) // 2, y - 10, font="Arial 12", fill="black")
+    idx = cnvs.index(lbl, tk.END)
+    cnvs.insert(lbl, idx, str(ship_dict["4"][4]) + "x")
+
     ship_dict["4"][0] = x
     ship_dict["4"][1] = y
     ship_dict["4"][2] = x_cnvs_size
     ship_dict["4"][3] = y_cnvs_size
+    ship_dict["4"][5] = lbl
 
     draw_interface_ship(x, y, x_cnvs_size, y_cnvs_size, False, cnvs)
 
@@ -252,10 +290,15 @@ def draw_interface(x_cnvs_size, y_cnvs_size, cnvs):
     x = x_cnvs_size - 10
     y = y_cnvs_size - 60
 
+    lbl = cnvs.create_text((x + x_cnvs_size) // 2, y - 10, font="Arial 12", fill="black")
+    idx = cnvs.index(lbl, tk.END)
+    cnvs.insert(lbl, idx, str(ship_dict["3"][4]) + "x")
+
     ship_dict["3"][0] = x
     ship_dict["3"][1] = y
     ship_dict["3"][2] = x_cnvs_size
     ship_dict["3"][3] = y_cnvs_size
+    ship_dict["3"][5] = lbl
 
     draw_interface_ship(x, y, x_cnvs_size, y_cnvs_size, False, cnvs)
 
@@ -263,10 +306,15 @@ def draw_interface(x_cnvs_size, y_cnvs_size, cnvs):
     x = x_cnvs_size - 10
     y = y_cnvs_size - 40
 
+    lbl = cnvs.create_text((x + x_cnvs_size) // 2, y - 10, font="Arial 12", fill="black")
+    idx = cnvs.index(lbl, tk.END)
+    cnvs.insert(lbl, idx, str(ship_dict["2"][4]) + "x")
+
     ship_dict["2"][0] = x
     ship_dict["2"][1] = y
     ship_dict["2"][2] = x_cnvs_size
     ship_dict["2"][3] = y_cnvs_size
+    ship_dict["2"][5] = lbl
 
     draw_interface_ship(x, y, x_cnvs_size, y_cnvs_size, False, cnvs)
 
@@ -285,6 +333,22 @@ def on_closing():
         TK.destroy()
 
 
+start_flag = 0
+my_field = []
+
+
+def start_game(event):
+    global start_flag, my_field
+    for i in range(len(bricks)):
+        mas = []
+        for g in range(len(bricks[i])):
+            mas.append(bricks[g][i].b_type)
+        my_field.append(mas)
+    canvas.delete("all")
+    print("start")
+    start_flag = 1
+
+
 TK.protocol("WM_DELETE_WINDOW", on_closing)
 TK.title("Ваше поле")
 TK.resizable(0, 0)
@@ -295,6 +359,30 @@ draw_playground(field_size_x, field_size_y, field_centering_x, field_centering_y
 draw_interface(size_canvas_x, size_canvas_y, canvas)
 canvas.pack()
 TK.update()
+
+while ship_dict["4"][4] + ship_dict["3"][4] + ship_dict["2"][4] > 1 or ship_placed == 0:
+    if app_running:
+        TK.update_idletasks()
+        TK.update()
+    time.sleep(0.005)
+
+canvas.create_rectangle(80, size_canvas_y - 60, size_canvas_x - 80, size_canvas_y - 20, fill="white",
+                        outline="black", tags="start")
+start_text = canvas.create_text((80 + size_canvas_x - 80) // 2, size_canvas_y - 40, font="Arial 12", fill="black",
+                                tags="start")
+idx = canvas.index(start_text, tk.END)
+canvas.insert(start_text, idx, "Start!")
+canvas.tag_bind("start", "<Button-1>", start_game)
+
+while start_flag == 0:
+    if app_running:
+        TK.update_idletasks()
+        TK.update()
+    time.sleep(0.005)
+
+
+canvas.create_rectangle(0, 0, size_canvas_x, size_canvas_y, fill="white")
+
 
 size_canvas2_x = 450
 size_canvas2_y = 450
