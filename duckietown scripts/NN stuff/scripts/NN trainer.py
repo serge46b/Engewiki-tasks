e9bbcdf3fb328b290103ece52577844b0a256f85
сергей <esmery@ngs.ru>
2021-05-02 17:20:28 +0700

    NN scripts updated (new datasets added)

--- duckietown scripts/NN stuff/scripts/NN trainer.py
+++ duckietown scripts/NN stuff/scripts/NN trainer.py
@@ -2,21 +2,31 @@ model_name = input("Enter model name: ")
 if model_name == "vgg_unet":
     from keras_segmentation.models.unet import vgg_unet
     model = vgg_unet(n_classes=50, input_height=320, input_width=640)
-elif model_name == "vgg_unet_dt":
+elif model_name == "vgg_unet_dt" or model_name == "vgg_unet_dt_mid_dataset" or model_name == "vgg_unet_dt_big_dataset" \
+        or model_name == "vgg_unet_dt_mid_dataset_dt4" or model_name == "vgg_unet_dt_big_dataset_dt5":
     from keras_segmentation.models.unet import vgg_unet
     model = vgg_unet(n_classes=2, input_height=480, input_width=640)
-elif model_name == "segnet_dt":
+elif model_name == "segnet_dt" or model_name == "segnet_dt_mid_dataset" or model_name == "segnet_dt_big_dataset"\
+        or model_name == "segnet_dt_mid_dataset_dt4" or model_name == "segnet_dt_big_dataset_dt5":
     from keras_segmentation.models.segnet import segnet
     model = segnet(n_classes=2, input_height=480, input_width=640)
 else:
     raise Exception("UnknownModelError")
 
-path = "C:/Users/Serg/Desktop/some_files/engewiki duckietown NN course/keras segmentation data/"
+path = "A:/Serge/Desktop/files/project files/engewiki duckietown NN course/keras segmentation data/"
 # dataset1/images_prepped_train/
-# dataset2/images/
+# dataset_dt/images/
+# dataset_dt2/images/
+# dataset_dt3/images/
+# dataset_dt4/images/
+# dataset_dt5/images/
 train_images_path = path + input("enter train images path: ")
 # dataset1/annotations_prepped_train/
-# dataset2/masks/
+# dataset_dt/masks/
+# dataset_dt2/masks/
+# dataset_dt3/masks/
+# dataset_dt4/masks/
+# dataset_dt5/masks/
 train_masks_path = path + input("enter train annotations path: ")
 
 model.train(
--- duckietown scripts/NN stuff/scripts/results visualizer.py
+++ duckietown scripts/NN stuff/scripts/results visualizer.py
@@ -4,16 +4,18 @@ model_name = input("Enter model name: ")
 if model_name == "vgg_unet":
     from keras_segmentation.models.unet import vgg_unet
     model = vgg_unet(n_classes=50, input_height=320, input_width=640)
-elif model_name == "vgg_unet_dt":
+elif model_name == "vgg_unet_dt" or model_name == "vgg_unet_dt_mid_dataset" or model_name == "vgg_unet_dt_big_dataset"\
+        or model_name == "vgg_unet_dt_mid_dataset_dt4" or model_name == "vgg_unet_dt_big_dataset_dt5":
     from keras_segmentation.models.unet import vgg_unet
     model = vgg_unet(n_classes=2, input_height=480, input_width=640)
-elif model_name == "segnet_dt":
+elif model_name == "segnet_dt" or model_name == "segnet_dt_mid_dataset" or model_name == "segnet_dt_big_dataset" \
+        or model_name == "segnet_dt_mid_dataset_dt4" or model_name == "segnet_dt_big_dataset_dt5":
     from keras_segmentation.models.segnet import segnet
     model = segnet(n_classes=2, input_height=480, input_width=640)
 else:
-    raise Exception("entered unknown model name")
+    raise Exception("UnknownModelError")
 
-path = "C:/Users/Serg/Desktop/some_files/engewiki duckietown NN course/keras segmentation data/"
+path = "A:/Serge/Desktop/files/project files/engewiki duckietown NN course/keras segmentation data/"
 
 model.load_weights(path + "tmp/" + model_name + "/weights/" + model_name + "_weights")
 
@@ -22,24 +24,27 @@ model.load_weights(path + "tmp/" + model_name + "/weights/" + model_name + "_wei
 classes = input("enter model classes: ").split(", ")
 
 # dataset1/images_prepped_test/0016E5_07965.png
-# dataset2/test/screenshot.1.png
+# dataset_dt/test/screenshot.1.png
+# dataset_dt2/test/img.1.png
+# dataset_dt3/test/img.1.png
+# dataset_dt4/test/img.1.png
 test_path = path + input("enter path to test image: ")
-results_path = path + "tmp/" + model_name + "/results/"
+results_name = path + "tmp/" + model_name + "/results/out_" + input("enter resulting image name: ")
 
 out = model.predict_segmentation(
     inp=test_path,
-    out_fname=results_path + model_name + "_out.png"
+    out_fname=results_name + model_name + ".png"
 )
 
-Image(results_path + model_name + "_out.png")
+Image(results_name + model_name + ".png")
 
 out_segment = model.predict_segmentation(
     inp=test_path,
-    out_fname=results_path + model_name + "_out_segment.png", overlay_img=True, show_legends=True,
+    out_fname=results_name + model_name + "_segment.png", overlay_img=True, show_legends=True,
     class_names=classes
 
 )
 
-Image(results_path + model_name + "_out_segment.png")
+Image(results_name + model_name + "_segment.png")
 
 print("resulting images successfully saved")
